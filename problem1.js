bd78d5c77b31121a7660d637b1ffcbff061905a3
Ashique Mohammed C <ashiquemohd25@gmail.com>
2022-09-27 20:39:09 +0530

    sample commit

--- problem1.js
+++ problem1.js
@@ -4,6 +4,26 @@ const obj={
     place:'malappuram'
 }
 
+const {name1,mark1}=obj
+
+console.log(name,mark);
+
+const obj1={
+    name:'Ashique',
+    mark:35,
+    place:'malappuram'
+}
+
+const {name2,mark2}=obj
+
+console.log(name,mark);
+
+const obj2={
+    name:'Ashique',
+    mark:35,
+    place:'malappuram'
+}
+
 const {name,mark}=obj
 
 console.log(name,mark);
